8d10be7bad6d47c61a7975b5aec15b67954f8c27
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-26 23:01:53 +0300

    Add get Movies api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movies';
 
 // For componentDidMount
 // useEffect(() => {
@@ -22,17 +24,51 @@ import PropTypes from 'prop-types';
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  
+
   console.log('render');
+
+  async function getMovies() {
+    // const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    // console.log(movies.data.data.movies);
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      'https://yts.mx/api/v2/list_movies.json?sort_by=rating'
+    );
+    // console.log(movies);
+    setMovies(movies);
+    setIsLoading(false);
+  }
 
   useEffect(() => {
     console.log('componentDidMount');
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
+    getMovies();
   }, []);
 
-  return <div className='App'>{isLoading ? 'Loading...' : 'Loaded...'}</div>;
+  return (
+    <div className='App'>
+      {isLoading
+        ? 'Loading...'
+        : movies.map((movie) => {
+            console.log(movie);
+            return (
+              <Movie
+                // key={movie.id}
+                // id={movie.id}
+                // title={movie.title}
+                // year={movie.year}
+                // summary={movie.summary}
+                // poster={movie.medium_cover_image}
+                {...movie}
+                key={movie.id}
+                poster={movie.medium_cover_image}
+              />
+            );
+          })}
+    </div>
+  );
 }
 
 export default App;
